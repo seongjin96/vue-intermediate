9235337d89cb901933ee3a829147496eb78197db
고 성진 <rtw456@jinhak.com>
2023-10-18 12:13:21 +0900

    icon class name 수정

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -2,7 +2,7 @@
   <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
     <span class="addContainer" v-on:click="addTodo">
-      <i class="fas fa-plus addBtn"></i>
+      <i class="fa-solid fa-plus addBtn"></i>
     </span>
   </div>
 </template>
