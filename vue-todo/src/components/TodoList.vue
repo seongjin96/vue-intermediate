97dc87ea5e28e3626b7f949f10009bbf7f0c919d
seongjin96 <w8348@naver.com>
2023-10-19 16:14:55 +0900

    TodoList 컴포넌트의 할 일 완료 기능 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <!-- <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i> -->
+        <span v-on:click="toggleComplete(todoItem, index)">
+          <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"></i>
+        </span>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fa-solid fa-trash"></i>
         </span>
@@ -22,13 +26,20 @@ export default {
     removeTodo: function(todoItem, index) {
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem) {
+      todoItem.completed = !todoItem.completed;
+      // localStorage의 데이터 갱신
+      // localStorage에는 update기능이 제공되지 않으므로 삭제 후 재추가
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
   created: function() {
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
     }
@@ -36,7 +47,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   ul {
     list-style-type: none;
     padding-left: 0px;
