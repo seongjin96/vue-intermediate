75277ff3a8d4b17f42f5ab84fc3296c8d07b8057
seongjin96 <w8348@naver.com>
2023-10-20 10:24:07 +0900

    todo-app 리팩토링

---
+++ vue-todo-refactoring/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- scoped - 컴포넌트 안에서만 유효한 스타일 속성 지정 -->
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
