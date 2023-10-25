97d9590fb6395c8716d81a29b00a42d0ff4bc2b0
seongjin96 <w8348@naver.com>
2023-10-25 17:56:28 +0900

    state 속성 적용

--- vue-todo-vuex/src/store/store.js
+++ vue-todo-vuex/src/store/store.js
@@ -4,8 +4,22 @@ import Vuex from 'vuex'
 // use - Vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때 사용
 Vue.use(Vuex);
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  },
+};
+
 export const store = new Vuex.Store({
   state: {
-    todoItems: []
+    todoItems: storage.fetch()
   }
 });
