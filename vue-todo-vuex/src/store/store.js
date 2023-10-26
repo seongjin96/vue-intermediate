56605dd1c27df0849d380f2d63ebde5fd3f9346d
seongjin96 <w8348@naver.com>
2023-10-26 15:20:54 +0900

    mutations로 할 일 추가, 완료, 모두 삭제 구현

--- vue-todo-vuex/src/store/store.js
+++ vue-todo-vuex/src/store/store.js
@@ -21,5 +21,28 @@ const storage = {
 export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch()
+  },
+  mutations: {
+    addOneItem(state, todoItem) {
+      const obj = {
+        completed: false, 
+        item: todoItem
+      };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      state.todoItems.push(obj);
+    },
+    removeOneItem(state, {todoItem, index}) {
+      localStorage.removeItem(todoItem.item);
+      state.todoItems.splice(index, 1);
+    },
+    toggleOneItem(state, {todoItem, index}) {
+      state.todoItems[index].completed = !state.todoItems[index].completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems(state) {
+      localStorage.clear();
+      state.todoItems = [];
+    }
   }
 });
