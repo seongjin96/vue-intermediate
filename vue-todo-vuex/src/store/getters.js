ddb1222bd450ace4aa876ca956a04c07a927502b
seongjin96 <w8348@naver.com>
2023-11-06 17:41:01 +0900

    스토어 속성 모듈화

---
+++ vue-todo-vuex/src/store/getters.js
@@ -0,0 +1,3 @@
+export const storedTodoItmes = (state) => {
+  return state.storedTodoItmes;
+}
--- vue-todo-vuex/src/store/store.js
+++ vue-todo-vuex/src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from './getters'
+import * as mutations from './mutations'
+
 // use - Vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때 사용
 Vue.use(Vuex);
 
@@ -22,32 +25,35 @@ export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch()
   },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems
-    }
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = {
-        completed: false, 
-        item: todoItem
-      };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, {todoItem, index}) {
-      localStorage.removeItem(todoItem.item);
-      state.todoItems.splice(index, 1);
-    },
-    toggleOneItem(state, {todoItem, index}) {
-      state.todoItems[index].completed = !state.todoItems[index].completed;
-      localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    },
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    }
-  }
+  getters,
+  mutations
+  // getters: {
+  //   storedTodoItems(state) {
+  //     return state.todoItems
+  //   }
+  // },
+
+  // mutations: {
+  //   addOneItem(state, todoItem) {
+  //     const obj = {
+  //       completed: false, 
+  //       item: todoItem
+  //     };
+  //     localStorage.setItem(todoItem, JSON.stringify(obj));
+  //     state.todoItems.push(obj);
+  //   },
+  //   removeOneItem(state, {todoItem, index}) {
+  //     localStorage.removeItem(todoItem.item);
+  //     state.todoItems.splice(index, 1);
+  //   },
+  //   toggleOneItem(state, {todoItem, index}) {
+  //     state.todoItems[index].completed = !state.todoItems[index].completed;
+  //     localStorage.removeItem(todoItem.item);
+  //     localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+  //   },
+  //   clearAllItems(state) {
+  //     localStorage.clear();
+  //     state.todoItems = [];
+  //   }
+  // }
 });
