9117a8bd4eaabe96a1bb2e5b56bfee9191c143d2
seongjin96 <w8348@naver.com>
2023-11-06 17:55:21 +0900

    스토어 모듈화

--- vue-todo-vuex/src/store/store.js
+++ vue-todo-vuex/src/store/store.js
@@ -1,32 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import * as getters from './getters'
-import * as mutations from './mutations'
+// store 속성 모듈화
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+
+import todoApp from './modules/todoApp'
 
 // use - Vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때 사용
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-    return arr;
-  },
-};
+// const storage = {
+//   fetch() {
+//     const arr = [];
+//     if (localStorage.length > 0) {
+//       for (let i = 0; i < localStorage.length; i++) {
+//         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+//           arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+//         }
+//       }
+//     }
+//     return arr;
+//   },
+// };
 
 export const store = new Vuex.Store({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters,
-  mutations
+  modules: {
+    todoApp
+  }
+
+  // store 속성 모듈화
+
+  // state: {
+  //   todoItems: storage.fetch()
+  // },
+  // getters,
+  // mutations
+
   // getters: {
   //   storedTodoItems(state) {
   //     return state.todoItems
